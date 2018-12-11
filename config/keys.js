727dd570c18a7b9a12a488f68905d3ffce497b78
Gliga Dumitru <gliga_dumitru@yahoo.com>
2018-12-11 21:21:42 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://dan:dan123@ds131784.mlab.com:31784/mern_shopping_list_gliga'
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//bodyparser middleware
+
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+
+mongoose.connect(db)
+    .then(() => console.log("MongoDB Connnected.."))
+    .catch(err => console.log(err));
+
+//Use routes
+
+app.use('/api/items', items);
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log(`Server is listening at ${port}`));
