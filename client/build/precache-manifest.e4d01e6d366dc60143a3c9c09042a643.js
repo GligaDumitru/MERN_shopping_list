cd61d2dfc04a42d4db1a08573420ca10a33ad154
Gliga Dumitru <gliga_dumitru@yahoo.com>
2019-06-25 16:52:46 +0300

    build done

---
+++ client/build/precache-manifest.e4d01e6d366dc60143a3c9c09042a643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44424c72527096fba764",
+    "url": "/static/js/main.44424c72.chunk.js"
+  },
+  {
+    "revision": "502c8dcd3c3e97b45a36",
+    "url": "/static/js/1.502c8dcd.chunk.js"
+  },
+  {
+    "revision": "44424c72527096fba764",
+    "url": "/static/css/main.f77422af.chunk.css"
+  },
+  {
+    "revision": "502c8dcd3c3e97b45a36",
+    "url": "/static/css/1.7de307ec.chunk.css"
+  },
+  {
+    "revision": "01aae17cbe82dfae4bfb2c8a0c24353c",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -10,19 +10,24 @@ const app = express();
 
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to Mongoaas
 
-mongoose.connect(db)
-    .then(() => console.log("MongoDB Connnected.."))
-    .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URI || db)
+  .then(() => console.log("MongoDB Connnected.."))
+  .catch(err => console.log(err));
 
 //Use routes
 
-app.use('/api/items', items);
+app.use("/api/items", items);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is listening at ${port}`));
